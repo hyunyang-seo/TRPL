e8b2ac3af9bb4e220ff749e22be2a1d25783428d
jadeitefirst <hyunyang.seo@gmail.com>
2023-10-03 01:22:58 +0900

    make ch01-01

---
+++ ch01-01/src/main.rs
@@ -0,0 +1,16 @@
+
+fn greet_world() {
+    println!("Hello, world!");
+    let southern_germany = "Grüß Gott!";
+    let korean = "안녕하세요!";
+    let regions = ["southern_germany", "korean"];
+
+    for region in regions.iter() {
+        println!("{}", &region);
+    }
+
+}
+
+fn main() {
+    greet_world()
+}
