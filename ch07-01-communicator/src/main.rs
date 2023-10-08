7678094caba9612e9981c03d3c6f08ffa4b58517
jadeitefirst <hyunyang.seo@gmail.com>
2023-10-08 23:06:42 +0900

    add test mod function call

--- ch07-01-communicator/src/main.rs
+++ ch07-01-communicator/src/main.rs
@@ -2,4 +2,6 @@ extern crate ch07_01_communicator;
 
 fn main() {
     ch07_01_communicator::client::connect();
+    ch07_01_communicator::network::connect();
+    ch07_01_communicator::network::server::connect();
 }
--- ch07-01-communicator/src/network/mod.rs
+++ ch07-01-communicator/src/network/mod.rs
@@ -1,6 +1,6 @@
 pub fn connect() {
-
+    println!("network::connect()");
 }
 
-mod server;
+pub mod server;
 
